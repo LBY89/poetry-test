c6dad4842621215ae1aba36e8e475a19b5800e8c
LBY89 <baoying_liu@hotmail.com>
2022-05-22 18:19:47 +0300

    added seperate route for 404

--- app.py
+++ app.py
@@ -20,7 +20,13 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.errorhandler(404)
+def page_not_found(e):
+    # your processing here
+    return 'something bad route'
+
+
 @app.route('/')
 @cross_origin()
 def serve():
